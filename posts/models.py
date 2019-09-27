0b2a07f017d22192bef93addb9be1cee46675905
boingo-00 <10793815+boingo-00@users.noreply.github.com>
2019-09-27 08:31:13 +0300

    новый файл:    posts/migrations/0008_auto_20190927_0816.py 	изменено:      posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+import string
 
 # Create your models here.
 
+def buildblock(size):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(size))
+
 def user_directory_path(post, filename):
     # file will be uploaded to MEDIA_ROOT/<username>/<filename>
-    return '{0}/{1}'.format(post.userid, filename)
+    return '{0}/{1}/{2}'.format(User.get_username, buildblock(10), filename)
 
 class post(models.Model):
-    userid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     desc = models.CharField(max_length=3000, verbose_name='Post Description')
     visible = models.BooleanField(default='True', verbose_name='Visible')
-    pubdate = models.DateTimeField('Published at:')
-
+    pubdate = models.DateTimeField('Published at:',auto_now=True)
 class album(models.Model):
-    postid = models.ForeignKey(post, on_delete=models.CASCADE)
-    photo = models.FileField(upload_to=user_directory_path)
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to=user_directory_path)
