4edc729aea0f8530f85a8469c13364134f2acf1b
boingo-00 <10793815+boingo-00@users.noreply.github.com>
2020-01-25 22:04:02 +0300

    Urlpatterns

--- herbarium/urls.py
+++ herbarium/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('posts/', include('posts.urls')),
+]
+urlpatterns += [
+    path('', RedirectView.as_view(url='/posts/', permanent=True)),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
