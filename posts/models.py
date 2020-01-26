033fa1c03ca234fb700f3dd03c4e6e72c3562a03
boingo-00 <10793815+boingo-00@users.noreply.github.com>
2020-01-26 10:50:02 +0300

    Readable names for objects

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/<username>/post_id_<id>/<filename>
-    return '{0}/post_id_{1}/{2}'.format(User.username, instance.post.id, filename)
+    return '{0}/post_id_{1}/{2}'.format(instance.user.username, instance.post.id, filename)
 
 class post(models.Model):
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,11 +11,14 @@ class post(models.Model):
     hidden  = models.BooleanField(verbose_name='Hidden post')
     pubdate = models.DateTimeField('Published at:', auto_now=True)
 
-    def getpubdate(self):
-        return post.pubdate
-    
+    def __str__(self):
+        return str(self.desc) if self.desc else "<No description>"
+
 
 class album(models.Model):
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
     post    = models.ForeignKey(post, default=None, on_delete=models.CASCADE)
     photo   = models.ImageField(upload_to=user_directory_path)
+
+    def __str__(self):
+        return "Album for "+str(self.post) if self.post else "<No description>"
