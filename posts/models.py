c577dfaadec3040d8a3ffa1a61bb7184804d3f35
boingo-00 <10793815+boingo-00@users.noreply.github.com>
2020-01-26 10:23:51 +0300

    User field is now cannot be null

--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/<username>/post_id_<id>/<filename>
-    return '{0}/post_id_{1}/{2}'.format(instance.user.username, instance.post.id, filename)
+    return '{0}/post_id_{1}/{2}'.format(User.username, instance.post.id, filename)
 
 class post(models.Model):
-    user    = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
     desc    = models.CharField(max_length=3000, verbose_name='Post Description')
     hidden  = models.BooleanField(verbose_name='Hidden post')
     pubdate = models.DateTimeField('Published at:', auto_now=True)
 
+    def getpubdate(self):
+        return post.pubdate
+    
+
 class album(models.Model):
-    user    = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
     post    = models.ForeignKey(post, default=None, on_delete=models.CASCADE)
     photo   = models.ImageField(upload_to=user_directory_path)
